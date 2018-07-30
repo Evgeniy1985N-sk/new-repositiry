8d388e88bb77418159bfb9b0a3b8fb0ca377a17a
Victor Volkov <vvolkov@smartru.com>
2018-07-30 14:52:24 +0300

    nyamki injected

---
+++ nyamki.js
@@ -0,0 +1,14 @@
+const nyamki=[
+  {type: 'cake', name: 'Praha', price: 300, pic: 'http://namnamra.com/public/r/200-300/210.jpg'},
+  {type: 'icecream', name: 'Plombir', price: 40, pic: 'https://s15.stc.all.kpcdn.net/share/i/12/9824087/inx960x640.jpg'},
+  {type: 'icecream', name: 'Leningradskoe', price: 50, pic: 'http://kormiles.ru/upload/resize_cache/iblock/466/450_450_16a9cdfeb475445909b854c588a1af844/466b77dd05a8072a13d67262833607cf.jpeg'},
+  {type: 'bake', name: 'Croissant', price: 40, pic: 'https://www.lidl.nl/catalog3media/nl/article/27988/gallery/preview/lg/27988_01.jpg'},
+  {type: 'cake', name: 'Tiramisu', price: 300, pic: 'https://www.fifteenspatulas.com/wp-content/uploads/2012/11/Tiramisu-Fifteen-Spatulas-1-640x424.jpg'},
+  {type: 'icecream', name: 'Krem-Brule', price: 300, pic: 'http://www.bbaum.ru/img/catalog/m372.jpg'},
+  {type: 'bake', name: 'Doughnut', price: 300, pic: 'https://cdn3.volusion.com/ztghq.pevcc/v/vspfiles/photos/_donut8-2T.jpg?1500887720'},
+  {type: 'bake', name: 'Cheese Stick', price: 300, pic: 'https://irecommend.img.c3.r-99.com/sites/default/files/imagecache/300o/product-images/684763/TUdlCEbMXxNSE4RHwCyOAw.jpg'},
+  {type: 'cake', name: 'Cheesecake', price: 300, pic: 'https://cdn.lifehacker.ru/wp-content/uploads/2018/03/new-york-cheesecake_1521023012-e1521023062144-630x314.jpg'},
+  {type: 'cake', name: 'Napoleon', price: 300, pic: 'https://e0.edimdoma.ru/data/recipes/0005/2730/52730-ed4_wide.jpg?1468463305'},
+  {type: 'icecream', name: 'Rojok', price: 300, pic: 'https://www.hesburger.ru/clients/hesburger/mellow_internal/imagefolder/ru//generated/dscf3282340xs_60.jpg'},
+  {type: 'icecream', name: 'Fruit Ice', price: 300, pic: 'http://vsedeserti.ru/wp-content/uploads/2015/03/Morozhenoe-fruktovyj-led1-360x240.jpg'},
+];
